8abb732f1fc37a91d19de110c884079ab11c348f
Deep Parmar <Parmardeep5305@gmail.com>
2026-04-20 19:40:17 +0530

    Add script to make all galleries public (SmugMug-style portfolio)

---
+++ server/scripts/make-all-galleries-public.js
@@ -0,0 +1,22 @@
+// Run this script with: node make-all-galleries-public.js
+// This will set all galleries to public visibility (like SmugMug)
+
+const { Gallery } = require('../models');
+const sequelize = require('../config/database');
+
+async function makeAllGalleriesPublic() {
+  try {
+    await sequelize.authenticate();
+    const [count] = await Gallery.update(
+      { visibility: 'public' },
+      { where: {} }
+    );
+    console.log(`Updated ${count} galleries to public.`);
+    process.exit(0);
+  } catch (err) {
+    console.error('Error updating galleries:', err);
+    process.exit(1);
+  }
+}
+
+makeAllGalleriesPublic();
